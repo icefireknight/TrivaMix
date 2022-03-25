426818973927c6c628f4676c94c4670ce7a8425d
Hazel Medina <jaxtonboom@gmail.com>
2022-03-25 02:40:50 -0700

    added read me, css, and cleaned up the code. Also added a timer.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # TrivaMix
-A guess the song game that uses Spotify's API
+A music trivia game that tests your knowledge of all different types of music.
+
+## Screenshots
+![Game board](https://i.imgur.com/ieW3X9u.png)
+![Game over](https://i.imgur.com/rldTj6C.png)
+![Game win](https://i.imgur.com/HX6SmIW.png)
+***
+## Technologies Used
+- JavaScript
+- HTML
+- CSS
+***
+## Getting Started
+Play the game [here](https://icefireknight.github.io/TrivaMix/)
+- As soon as you load in you have 75 seconds to guess as many songs as you can.
+- Press the play button to listen to the song.
+- You must guess the song and artist before you hit submit.
+- If your answer was right, you will get a new song.
+***
+## Next Steps
+- Implement Spotify API so an infinite number of songs can be used in the game
+- Add different time options
+- Show the timer
+- Redesign the look of the game.
--- js/app.js
+++ js/app.js
@@ -1,116 +1,171 @@
-
+//create array of songs
 const songs = [
-    {
+  {
     name: 'agoraphobic',
     artist: 'corpse',
-    songID: '6ATqL4f1nFWwASrWi8nF6e'
-},
-{
+    songID: '6ATqL4f1nFWwASrWi8nF6e',
+  },
+  {
     name: 'corso',
     artist: 'tyler the creator',
-    songID: '46oHvXwpEZOFX518we1nJD'
-},
-{
+    songID: '46oHvXwpEZOFX518we1nJD',
+  },
+  {
     name: 'alien blues',
     artist: 'vundabar',
-    songID: '6UjeFOCGYgMpBUtqKg1Je3'
-},
-{
+    songID: '6UjeFOCGYgMpBUtqKg1Je3',
+  },
+  {
     name: 'no children',
     artist: 'the mountain goats',
-    songID: '5cxnSTLzGD1t9xcdmJYFVB'
-}
+    songID: '5cxnSTLzGD1t9xcdmJYFVB',
+  },
+  {
+    name: 'say so',
+    artist: 'doja cat',
+    songID: '3Dv1eDb0MEgF93GpLXlucZ',
+  },
+  {
+    name: 'thats what i want',
+    artist: 'lil nas x',
+    songID: '0e8nrvls4Qqv5Rfa2UhqmO',
+  },
+  {
+    name: 'hayloft',
+    artist: 'mother mother',
+    songID: '2kS6td1yvmpNgZTt1q5pQq',
+  },
+  {
+    name: 'venom',
+    artist: 'little simz',
+    songID: '3A0ITFj6kbb9CggwtPe55f',
+  },
+  {
+    name: 'buttercup',
+    artist: 'jack stauber',
+    songID: '2mlGPkAx4kwF8Df0GlScsC',
+  },
+  {
+    name: 'soap',
+    artist: 'melanie martinez',
+    songID: '03f7xZmt2wHCIDJBFPK8G4',
+  },
+  {
+    name: 'be nice to me',
+    artist: 'the front bottoms',
+    songID: '2mQqKFxQ6PDSKyFOCSLrXc',
+  },
+  {
+    name: 'brutal',
+    artist: 'olivia rodrigo',
+    songID: '6SRsiMl7w1USE4mFqrOhHC',
+  },
+  {
+    name: 'teenagers',
+    artist: 'my chemical romance',
+    songID: '7j31rVgGX9Q2blT92VBEA0',
+  },
+  {
+    name: 'basket case',
+    artist: 'green day',
+    songID: '6L89mwZXSOwYl76YXfX13s',
+  },
+  {
+    name: 'toxicity',
+    artist: 'system of a down',
+    songID: '0snQkGI5qnAmohLE7jTsTn',
+  },
+  {
+    name: 'brass monkey',
+    artist: 'beastie boys',
+    songID: '2tY1gxCKslfXLFpFofYmJQ',
+  },
 ]
+//randomize a given array
 function shuffle(array) {
-    let currentIndex = array.length, randomIndex;
+  let currentIndex = array.length,
+    randomIndex
 
-    while (currentIndex != 0) {
-        randomIndex = Math.floor(Math.random() * currentIndex);
-        currentIndex--;
+  while (currentIndex != 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex)
+    currentIndex--
+    ;[array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ]
+  }
 
-        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
+  return array
 }
+//randomize our array
+shuffle(songs)
 
-shuffle(songs);
-console.log(songs);
+//grab information about the first song in our random array
+let songChoice = songs[0]
+let choiceID = songChoice.songID
+let choiceName = songChoice.name
+let choiceArtist = songChoice.artist
 
-let songChoice = songs[0];
-let choiceID = songChoice.songID;
-let choiceName = songChoice.name;
-let choiceArtist = songChoice.artist;
+//initialize our score
+let score = 0
 
-let score = 0;
-document.getElementById('score').innerText = `Score:${score}`;
+//put our score on the screen as a variable so it can change
+document.getElementById('score').innerText = `Score:${score}`
+//embed the first song in the array onto the screen using spotify embed
+document.getElementById(
+  'music',
+).innerHTML = `<iframe id="song" style="border-radius:75px" src="https://open.spotify.com/embed/track/${choiceID}?utm_source=generator" width="66%" height="90" frameBorder="0" allowfullscreen="" autoplay="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`
 
-document.getElementById('music').innerHTML = `<iframe id="song" style="border-radius:75px" src="https://open.spotify.com/embed/track/${choiceID}?utm_source=generator" width="4%" height="80" frameBorder="0" allowfullscreen="" autoplay="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`;
+//check the text input from player
+function checkGuess(array) {
+  let songGuess = document.getElementById('name').value
+  let artistGuess = document.getElementById('artist').value
+  choiceName = songs[0].name
+  choiceArtist = songs[0].artist
 
+  //if the song input and artist input are correct
+  if (
+    songGuess.toLowerCase() === choiceName.toLowerCase() &&
+    artistGuess.toLowerCase() === choiceArtist.toLowerCase()
+  ) {
+    //add 1 to our score
+    score = score + 1
+    //update score on screen
+    document.getElementById('score').innerText = `Score:${score}`
+    //remove first item in array
+    array.shift()
+    //grab information from next song in array
+    let songChoice = songs[0]
 
-function checkGuess(array){
-let songGuess = document.getElementById('name').value;
-let artistGuess = document.getElementById('artist').value;
-choiceName = songs[0].name;
-choiceArtist = songs[0].artist;
+    let choiceID = songChoice.songID
+    //replace song on screen with next song
+    document.getElementById(
+      'music',
+    ).innerHTML = `<iframe id="song" style="border-radius:75px" src="https://open.spotify.com/embed/track/${choiceID}?utm_source=generator" width="66%" height="90" frameBorder="0" allowfullscreen="" autoplay="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`
+  }
 
-console.log(array, score);
-if (songGuess.toLowerCase() === choiceName.toLowerCase() && artistGuess.toLowerCase() === choiceArtist.toLowerCase()){
-    console.log('correct');
-    score = score + 1;
-    document.getElementById('score').innerText = `Score:${score}`;
-    array.shift();
-    let songChoice = songs[0];
-    console.log(songChoice);
-    let choiceID = songChoice.songID;
-    
-    document.getElementById('music').innerHTML = `<iframe id="song" style="border-radius:75px" src="https://open.spotify.com/embed/track/${choiceID}?utm_source=generator" width="4%" height="80" frameBorder="0" allowfullscreen="" autoplay="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`;
-    
-    
+  return score
 }
-
-return score;
+//stop the game
+function stopTime() {
+  //remove all elements except for score and title
+  document.getElementById('music').remove()
+  document.getElementById('name').remove()
+  document.getElementById('artist').remove()
+  document.getElementById('guessBtn').remove()
+  //move score
+  document.getElementById('score').style.gridColumnStart = 3
+  document.getElementById('score').style.gridRowStart = 3
+  //use score to decide win or lose
+  if (score < 4) {
+    document.getElementById(
+      'score',
+    ).innerText = `You guessed ${score} songs right. Better luck next time.`
+  } else {
+    document.getElementById(
+      'score',
+    ).innerText = `You guessed ${score} songs right. Great job!`
+  }
 }
-
-// function updateTime(){
-//     timeValue = timeValue - 1;
-//     document.getElementById('time').innerText = `Time Left:${timeValue}`;
-//     console.log('check');
-// }
-
-function stopTime(){
-    // clearInterval(myInterval);
-    document.getElementById('music').remove();
-    document.getElementById('name').remove();
-    document.getElementById('artist').remove();
-    document.getElementById('guessBtn').remove();
-    console.log('adios')
-}
-
-
-    const stopGame = setTimeout(stopTime, 60000);
-
-
-// function getRandomInt(max) {
-//     return Math.floor(Math.random() * max);
-//   }
-
-// function playSong(){
-//     let currentSong = document.getElementById('song'); 
-//     currentSong.remove();
-//     let songInt = getRandomInt(songs.length);
-//     let songChoice = songs[songInt];
-//     let choiceID = songChoice.songID;
-//     document.getElementById('music').innerHTML += `<iframe id="song" style="border-radius:75px" src="https://open.spotify.com/embed/track/${choiceID}?utm_source=generator" width="4%" height="80" frameBorder="0" allowfullscreen="" autoplay="true" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>`;
-//     return songChoice;
-
-// }
-
-    //currentSong = document.getElementById('song');
-    // let songGuess = document.getElementById('name').value;
-    // let artistGuess = document.getElementById('artist').value;
-    // if (songGuess.length>0 && artistGuess.length>0 && songGuess.toLowerCase() === songName && artistGuess.toLowerCase() === artistName){
-    //     score = score + 1;
-    //     playSong();
-    // }
-    // console.log(songGuess);
+//set timer to 75 seconds before the game stops
+const stopGame = setTimeout(stopTime, 75000)
